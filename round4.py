59f5b66df13c506b464ee3f32a661875be25e575
Xhiundu <160715476+japheth-shiundu@users.noreply.github.com>
2025-03-05 12:07:04 +0300

    Create round4.py

---
+++ round4.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Style
+import random
+import time
+
+def run_round():
+    terminate_at = round(random.uniform(4.00, 6.00), 2)
+    current_number = 1.00
+    while current_number <= terminate_at:
+        print(Fore.WHITE + f"\rCounting: {current_number:.2f}", end="", flush=True)
+        time.sleep(0.1)
+        current_number += 0.01
+    print(Fore.MAGENTA + f"\rTerminated at {current_number:.2f}""x" + Style.RESET_ALL)
+
+
+
+
